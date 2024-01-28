a2a11aa7f80bd7b51aa05b8cc2e2a789a517a131
Ajit2576 <itajitkumar0@gmail.com>
2024-01-29 02:38:00 +0530

    new

--- models/products.model.js
+++ models/products.model.js
@@ -24,6 +24,30 @@ const productSchema = mongoose.Schema({
     descriptions:{
         type: String,
         required: true
+    },
+    colors:{
+        type: Array,
+        required:true
+    },
+    company:{
+        type: String,
+        required: true
+    },
+    stock:{
+        type: Number,
+        required: true
+    },
+    stars:{
+        type: Number,
+        required: true
+    },
+    reviews:{
+        type: Number,
+        required: true
+    },
+    size:{
+        type: Array,
+        required: true
     }
 })
 
