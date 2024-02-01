6add430bc91b8c50ea539f0f74d4e778a13e532f
Ajit2576 <itajitkumar0@gmail.com>
2024-02-01 17:20:08 +0530

    1.2

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -2,19 +2,28 @@ import Product from "../models/products.model.js";
 
 export const createProducts = async (req, res) => {
     try {
-      let newProduct =  await Product.create(req.body)
-      res.status(200).json(newProduct)
+        let newProduct = await Product.create(req.body)
+        res.status(200).json(newProduct)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
-export const getProducts = async (req, res) =>{
+export const getProducts = async (req, res) => {
+    const { page } = req.query
+    const LIMIT = 9
+    const startIndex = (Number(page) - 1) * LIMIT; 
+    const total = await Product.countDocuments({})
+
     try {
-        let allProduct = await Product.find().sort({createdAt: -1})
-         res.status(200).json(allProduct)
+        let allProduct = await Product.find().sort({ createdAt: -1 }).limit(LIMIT).skip(startIndex)
+        res.status(200).json({
+            data: allProduct,
+            currentPage: Number(page),
+            numberOfPages: Math.ceil(total / LIMIT),
+        })
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
@@ -23,6 +32,6 @@ export const getProduct = async (req, res) => {
         let singleProduct = await Product.findById(req.params.id)
         res.status(200).json(singleProduct)
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
